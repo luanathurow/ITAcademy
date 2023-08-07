8038c2e84c30883ad61388803d51fc4889d40cb7
Thurow <luana.thurow@dellteam.com>
2023-08-07 13:55:14 -0300

    Trabalhando com tipos-valor fundamentais byte, int e long

---
+++ Aula01/Laboratorio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Laboratorio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b;
+            b = Byte.MaxValue;
+            Console.WriteLine("Valor máximo de byte: " + b);
+
+            int i;
+            i = int.MaxValue;
+            Console.WriteLine("Valor máximo de int: " + i);
+
+            long l;
+            l = long.MaxValue;
+            Console.WriteLine("Valor máximo de long: " + l);
+        }
+    }
+}
