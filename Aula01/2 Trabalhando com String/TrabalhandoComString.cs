de59a4c21cc5eb627604a0d90522750a90caaa9a
Thurow <luana.thurow@dellteam.com>
2023-08-07 23:04:09 -0300

    Trabalhando com saídas de Strings

---
+++ Aula01/2 Trabalhando com String/TrabalhandoComString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TrabalhandoComString
+{
+    class TiposVariaveis
+    {
+        static void Main(string[] args)
+        {
+            string strPrimeira = "Alo ";
+            string strSegunda = "Mundo";
+
+            string strTerceira = strPrimeira + strSegunda;
+
+            Console.WriteLine(strTerceira);
+
+            int cchTamanho = strTerceira.Length;
+
+            string strQuarta = "Tamanho = " + cchTamanho.ToString();
+            Console.WriteLine(strQuarta);
+
+            Console.WriteLine(strTerceira.Substring(0, 5));
+        }
+    }
+}
